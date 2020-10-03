7ed8078b18e0c18d69e9c1ad5b4169f1a5bb8311
Marcin Pogorzelski <pogorzelski.marcin@gmail.com>
2020-10-03 20:08:40 +0200

    this is working poc

--- src/main/java/com/mp/exercise/StartApplication.java
+++ src/main/java/com/mp/exercise/StartApplication.java
@@ -1,19 +1,26 @@
 package com.mp.exercise;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.hadoop.fs.FsShell;
 
 @SpringBootApplication
-public class StartApplication {
+public class StartApplication implements CommandLineRunner {
+
+    @Autowired
+    private FsShell shell;
+
+    @Override
+    public void run(String... strings) {
+        shell.mkdir("/data/marcin/example");
+        shell.lsr("/data").forEach(fileStatus -> System.out.println(fileStatus.getPath().toString()));
+        shell.rm(true,true,"/data/marcin");
+    }
+
+
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
-//        ApplicationContext ctx = new AnnotationConfigApplicationContext(ConfigClass.class);
-//
-//        System.out.println(ctx.containsBean("configClass"));
-//        System.out.println(ctx.containsBean("hadoopConfiguration"));
-//        System.out.println(ctx.containsBean("fsShell"));
-
-//        AppRunner appRunner = (AppRunner) ctx.getBean("appRunner");
-//        appRunner.run("");
     }
 }
